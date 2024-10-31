86d7d8468968a137f1ee0ea547233f415efc8f85
Fran <francisco.flores5@educa.madrid.org>
2024-10-31 10:37:18 +0100

    Fase 3: Migración a PHP

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Inicio - Fútbol</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Bienvenidos al sitio de Fútbol</h1>
+    <nav>
+        <a href="equipos.php">Equipos</a>
+        <a href="jugadores.php">Jugadores</a>
+    </nav>
+    <p>Descubre los mejores equipos y jugadores del mundo del fútbol.</p>
+</body>
+</html>
